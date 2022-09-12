1e08828b5f2fb508a67e83e244a01fd04d699d1b
Nikita Shirokopetlev <35659452+FlameDanita@users.noreply.github.com>
2022-09-12 15:07:04 +0300

    init v2
    gl hf

--- AASM6_Examples/AASM6_CppLibExample/CallFunctionExample.cpp
+++
@@ -1,9 +0,0 @@
-#include "..\..\AASM6_Library\AASM6\AASM6.h"
-
-void DemoFunc()
-{
-	float Res;
-	AASM6::InputComplex x_1{ 0.0f, 0.0f }, x_2{ 0.0f, 0.0f };
-	AASM6::get_Cyalf_ConCil(Res, 2, 2, 2, x_1, x_2);
-	AASM6::get_Cyalf_OgiCil(Res, 2, 2, 2, x_1, x_2);
-}
